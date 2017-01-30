97932a4549434033809a49e678c6aba9540bf697
Anton Bourosu <antonbourosu00@gmail.com>
2017-01-30 17:10:43 +0200

    Add comment to current code

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 
 
 class App extends React.Component{
+    // If component need to have a constructor, in constructor net to call super(); method to have access to this
+    // If you want to use props in constructor you would call super(props); Otherwise you cant call props in super method
     constructor(){
         super();
         this.state = {
@@ -11,6 +13,7 @@ class App extends React.Component{
     }
 
     update(e){
+        // This is the primary method you use to trigger UI updates from event handlers and server request callbacks.
         this.setState({txt: e.target.value})
     }
 
@@ -18,7 +21,6 @@ class App extends React.Component{
         let txt = this.props.txt;
         return(
             <div>
-
                 <h1>{txt}</h1>
                 <bold>{this.props.cat}</bold>
                 <h1>{this.state.txt} - {this.props.cat}</h1>
@@ -29,17 +31,21 @@ class App extends React.Component{
     }
 }
 
+// As your app grows, you can catch a lot of bugs with typeChecking.
 App.propTypes = {
     txt : React.PropTypes.string,
     cat : React.PropTypes.number.isRequired
 }
 
+// defaultProps can be defined as a property on the component class itself, to set the default props for the class.
 App.defaultProps = {
     txt : "Default text",
     cat : 1
 }
 
+// A widget component used in App component.
 const Widget = (props) =>
     <input type="text" onChange={props.update}/>
+
 
 export default App;
